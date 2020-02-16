e960a749089d079b0188c64d95e88b544663af71
Dolly Chhabriya <dolly.c@altius.cc>
2020-02-17 03:33:39 +0530

    Updates

---
+++ precache-manifest.f58da2bdbdfce172babfde8ce257ab34.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67748a3beeaacc0d74abf955437751c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b39d2987e3c32d2824",
+    "url": "/static/css/main.d143daa1.chunk.css"
+  },
+  {
+    "revision": "6df7983e55688c8fc2e2",
+    "url": "/static/js/2.ac138660.chunk.js"
+  },
+  {
+    "revision": "b00cdf5170fa1e398a5424a6111c3bc4",
+    "url": "/static/js/2.ac138660.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3b39d2987e3c32d2824",
+    "url": "/static/js/main.d8deb134.chunk.js"
+  },
+  {
+    "revision": "f6fcd36d8a7c7af34711",
+    "url": "/static/js/runtime-main.398e9359.js"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "/static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "/static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "/static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  },
+  {
+    "revision": "ff423a4251cf2986555523dfe315c42b",
+    "url": "/static/media/glyphicons-halflings-regular.ff423a42.svg"
+  }
+]);
